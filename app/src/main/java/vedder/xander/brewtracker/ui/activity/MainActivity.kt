6c0ad97ba58b4d410f4a4c2415f71617a8e0f7cc
Xander Vedder <xander.vedd@gmail.com>
2020-07-30 16:07:27 +0200

    Add persistency to fragments
    - No longer creates new fragments, rather, reuses them now.
    - Also added a small transition when tapping the buttons.

--- app/src/main/java/vedder/xander/brewtracker/ui/activity/MainActivity.kt
+++ app/src/main/java/vedder/xander/brewtracker/ui/activity/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentTransaction
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import vedder.xander.brewtracker.R
 import vedder.xander.brewtracker.ui.fragment.BrewFragment
@@ -11,26 +12,37 @@ import vedder.xander.brewtracker.ui.fragment.HomeFragment
 import vedder.xander.brewtracker.ui.fragment.RecipeFragment
 
 class MainActivity : AppCompatActivity() {
+    private val homeFragment = HomeFragment()
+    private val recipeFragment = RecipeFragment()
+    private val brewFragment = BrewFragment()
+
+    // HomeFragment should be the first one to load
+    private var activeFragment: Fragment = homeFragment
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottom_navigation_view)
         bottomNavigationView.setOnNavigationItemSelectedListener { item: MenuItem ->
             when (item.itemId) {
-                R.id.item_home -> loadFragment(HomeFragment())
-                R.id.item_recipe -> loadFragment(RecipeFragment())
-                R.id.item_brew -> loadFragment(BrewFragment())
+                R.id.item_home -> replaceFragment(homeFragment)
+                R.id.item_recipe -> replaceFragment(recipeFragment)
+                R.id.item_brew -> replaceFragment(brewFragment)
                 else -> false
             }
         }
-        loadFragment(HomeFragment())
+        replaceFragment(activeFragment)
     }
 
-    private fun loadFragment(fragment: Fragment): Boolean {
+    private fun replaceFragment(fragment: Fragment): Boolean {
         supportFragmentManager
                 .beginTransaction()
+                .hide(activeFragment)
+                .show(fragment)
                 .replace(R.id.fragment_container, fragment)
+                .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN)
                 .commit()
+        activeFragment = fragment
         return true
     }
 }
